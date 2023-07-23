b556a35284232f43bbb4ab09a551d87cc870e42f
n1320447 <goveanestor@gmail.com>
2023-07-23 15:11:06 -0500

    Author: Nestor Govea: added a menu to repeat until user chooses 9.

--- Library.java
+++ Library.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Library {
+    Scanner scanner;
     Library(){
-        System.out.print("new library made");
+        scanner = new Scanner(System.in);
+        System.out.println("new library made");
     }
+
+    public void libaryMenu(){
+        int choice = 0;
+        while (choice != 9) {
+        System.out.println("Please choose what do by the number.");
+        System.out.println("1. Add User");
+        System.out.println("2. View which items are checked out.");
+        System.out.println("9. Exit");
+        
+        choice = getUserChoice();
+
+
+        System.out.println("You selected option: " + choice);
+
+        
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+
+                break;
+            case 9:
+                System.out.println("Exiting the library system. Goodbye!");
+                break;
+            default:
+                System.out.println("Invalid option. Please choose a valid number.");
+                break;
+            }
+        }
+    }
+
+    private int getUserChoice(){
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        return choice;
+    }
+
 }
