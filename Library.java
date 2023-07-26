c959554456bc9687b4645a0f96df3d64fe65a7a8
n1320447 <goveanestor@gmail.com>
2023-07-26 15:48:00 -0500

    Working on makeing sure creating an Item is working well. Still in progress.

--- Library.java
+++ Library.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Library {
     Scanner scanner;
     int userCount = 0;
+    int itemCount = 0;
     Library(){
         scanner = new Scanner(System.in);
         System.out.println("new library made");
@@ -17,6 +18,8 @@ public class Library {
         System.out.println("1. Create Account");
         System.out.println("2. View which items are checked out.");
         System.out.println("3. Get how many Users are in Library System.");
+        System.out.println("4. Checkout an Item");
+        System.out.println("5. Add item to library (book or audio/visual)");
         System.out.println("9. Exit");
         
         choice = getUserChoice();
@@ -28,6 +31,7 @@ public class Library {
         switch (choice) {
             case 1:
                 System.out.println(" Create Account.");
+                System.out.println();
                 String fname = "";
                 String lname = "";
                 System.out.println("enter first name");
@@ -44,14 +48,42 @@ public class Library {
 
                 break;
             case 3:
-                System.out.println("User count is: " + getNumberOfUsers());
+                System.out.println(" User count is: " + getNumberOfUsers());
+                System.out.println();
+                break;
+            case 5:
+                System.out.println(" Add Item.");
+                System.out.println();
+
+                
+                String lastName = "";
+               
+                System.out.println("please enter last name of author/director.");
+                scanner = new Scanner(System.in);
+                lastName = scanner.nextLine();
+
+                System.out.println("Please choose if item will be a book or audio/video item.");
+                System.out.println("1. Book");
+                System.out.println("2. Audio/Video");
+
+                scanner = new Scanner(System.in);
+                String type = "";
+                if(scanner.nextInt() == 1){
+                    Item newItem = new Item(lastName, choice, Item.ItemType.BOOK);
+                }
+
+                
+
+
                 break;
             case 9:
                 System.out.println(" Exiting the library system. Goodbye!");
+                System.out.println();
                 break;
             
             default:
-                System.out.println("Invalid option. Please choose a valid number.");
+                System.out.println(" Invalid option. Please choose a valid number.");
+                System.out.println();
                 break;
             }
         }
